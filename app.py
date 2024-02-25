28a27790da77a53274f2183cdd835139a1902c4b
killian31 <80256683+killian31@users.noreply.github.com>
2024-02-25 12:36:56 +0100

    feat: pbar

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import subprocess
+import warnings
 
 import cv2
 import gradio as gr
@@ -6,12 +7,15 @@ import numpy as np
 import torch
 import whisper
 from pydub import AudioSegment
+from tqdm import tqdm
 
 
-def generate_video_cv2(audio_path, language, lag):
+def generate_video_cv2(audio_path, language, lag, progress=gr.Progress()):
 
     # Transcribe audio
+    progress(0.0, "Transcribing audio...")
     result = model.transcribe(audio_path, language=language)
+    progress(0.30, "Audio transcribed!")
 
     # Video settings
     fps = 6
@@ -27,7 +31,19 @@ def generate_video_cv2(audio_path, language, lag):
     video = cv2.VideoWriter(output_path, fourcc, fps, frame_size)
 
     # Prepare video frames from transcription segments
+    if lag > 0:
+        duration = lag
+        frames_count = int(duration * fps)
+        for _ in range(frames_count):
+            img = np.full(
+                (frame_size[1], frame_size[0], 3), background_color, dtype=np.uint8
+            )
+            video.write(img)
+    total_segments = len(result["segments"])
+    running_progress = 0.0
+
     for segment in result["segments"]:
+        running_progress += 0.4 / total_segments
         text = segment["text"]
         duration = segment["end"] - segment["start"]
         frames_count = int(duration * fps)
@@ -49,17 +65,20 @@ def generate_video_cv2(audio_path, language, lag):
             )
 
             video.write(img)
+        progress(min(0.3 + running_progress, 0.7), "Generating video frames...")
 
     video.release()
+    progress(0.7, "Video frames generated!")
 
     # Add lag if specified
     if lag > 0:
-        audio = AudioSegment.silent(duration=lag * 1000) + AudioSegment.from_file(
-            audio_path
+        audio = AudioSegment.from_file(audio_path) + AudioSegment.silent(
+            duration=lag * 1000
         )
     else:
         audio = AudioSegment.from_file(audio_path)
 
+    progress(0.75, "Merging audio and video...")
     # Export audio
     audio_path_modified = "./modified_audio.mp3"
     audio.export(audio_path_modified, format="mp3")
@@ -68,11 +87,13 @@ def generate_video_cv2(audio_path, language, lag):
     final_output_path = "./final_transcribed_video.mp4"
     cmd = f"ffmpeg -y -i {output_path} -i {audio_path_modified} -c:v copy -c:a aac {final_output_path}"
     subprocess.run(cmd.split())
+    progress(1, "Done!")
 
     return final_output_path
 
 
 if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
     # Load Whisper model
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model = whisper.load_model("base", device=device)
